f83ab1f28f0375f0036aafe2656f6703d000e2e5
Edan <edan.leibo@gmail.com>
2019-07-18 14:59:09 +0300

    initial commit

---
+++ Board.ts
@@ -0,0 +1,95 @@
+export class Board {
+    isCompleted: boolean;
+    board: string[][];
+
+    constructor(n: number = 3, m: number = 3) {
+        if (n !== m || n < 3) {
+            console.log("Illegal board size");
+            console.log("Creating a default 3X3 board");
+            n = 3;
+            m = 3;
+        }
+
+        this.isCompleted = false;
+        this.board = [];
+        for (let i = 0; i < n; i++) {
+            this.board[i] = [];
+        }
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < m; j++) {
+                this.board[i][j] = '*';
+            }
+        }
+    }
+
+    isOccupiedCell(row: number, col: number): boolean {
+        return this.board[row][col] !== '*';
+    }
+
+    print() {
+        let boardString = "";
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                boardString += ` ${this.board[i][j]}`;
+            }
+            boardString += '\n'
+        }
+        console.log(boardString);
+    }
+
+    updateBoard(row: number, col: number, sign: string) {
+        this.board[row][col] = sign;
+        this.isCompleted = (this.checkRow(row) || this.checkCol(col) || this.mainDiagonal() || this.antiDiagonal()) ? true : false;
+    }
+
+    private checkRow(row: number): boolean {
+        let firstVal = this.board[row][0];
+        let ans = true;
+        for (let j = 0; j < this.board[row].length; j++) {
+            if (this.board[row][j] === '*' || this.board[row][j] !== firstVal) {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    private checkCol(col: number): boolean {
+        let firstVal = this.board[0][col];
+        let ans = true;
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i][col] === '*' || this.board[i][col] !== firstVal) {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    private mainDiagonal(): boolean {
+        let firstVal = this.board[0][0];
+        let ans = true;
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i][i] === '*' || this.board[i][i] !== firstVal) {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    private antiDiagonal(): boolean {
+        let j = this.board[0].length - 1;
+        let firstVal = this.board[0][j];
+        let ans = true;
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i][j] === '*' || this.board[i][j] !== firstVal) {
+                ans = false;
+                break;
+                j--;
+            }
+        }
+        return ans;
+    }
+}
---
+++ Game.ts
@@ -0,0 +1,74 @@
+import { GameStatus } from './Utils';
+import { Board } from './Board';
+import { Player } from './Player';
+
+export class Game {
+    board: Board;
+    status: GameStatus;
+    currentPlayer: Player;
+    playerOne: Player;
+    playerTwo: Player;
+    history: string[];
+
+    constructor(n: number, m: number) {
+        this.status = GameStatus.InProgress;
+        this.board = new Board(n, m);
+        this.history = [];
+    }
+
+    addPlayer(player: Player) {
+        if (!this.playerOne) {
+            this.playerOne = player;
+            this.currentPlayer = player;
+        } else if (!this.playerTwo) {
+            this.playerTwo = player;
+        }
+        else {
+            console.log("Two players have already been added");
+        }
+    }
+
+    printSummary() {
+        if (!this.history) {
+            console.log("Game is in progress");
+            return;
+        }
+
+        switch (this.status) {
+            case GameStatus.InProgress:
+                console.log("Game is in progress");
+                console.log("Moves history:");
+                console.log(this.history.join('\n'));
+                break;
+            case GameStatus.Completed:
+                console.log(`${this.currentPlayer.name} won!`);
+                console.log(this.history.join('\n'));
+                break;
+        }
+    }
+
+    nextMove(row: number, col: number): boolean {
+        //Check if it is a bad move
+        if (this.status === GameStatus.Completed || this.board.isOccupiedCell(row, col)) return false;
+
+        //add to moves history
+        this.history.push(`${this.currentPlayer.name} placed ${this.currentPlayer.sign} in (${row},${col})`);
+        this.board.updateBoard(row, col, this.currentPlayer.sign);
+        if (this.board.isCompleted) {
+            this.status = GameStatus.Completed;
+        }
+        else {
+            if (this.currentPlayer.name === this.playerOne.name) {
+                this.currentPlayer = this.playerTwo;
+            }
+            else {
+                this.currentPlayer = this.playerOne;
+            }
+        }
+        return true;
+    }
+
+
+}
+
+
